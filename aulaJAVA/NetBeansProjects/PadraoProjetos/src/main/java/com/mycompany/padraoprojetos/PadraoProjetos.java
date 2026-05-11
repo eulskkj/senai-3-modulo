ae122ca0f6cf380ca098f5d5d422cd71697a8144
Luis <pikapaybeludo@gmail.com>
2026-05-11 16:27:32 -0300

    Add files via upload

---
+++ aulaJAVA/NetBeansProjects/PadraoProjetos/src/main/java/com/mycompany/padraoprojetos/PadraoProjetos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.padraoprojetos;
+
+/**
+ *
+ * @author aluno.den
+ */
+public class PadraoProjetos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
