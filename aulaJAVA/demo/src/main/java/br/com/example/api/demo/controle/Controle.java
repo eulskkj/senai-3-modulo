747059433b49242d60c34fa24eab0c09966d1217
eulskkj <lo8244641@gmail.com>
2026-06-09 13:27:49 -0300

    salvar

--- aulaJAVA/demo/src/main/java/br/com/example/api/demo/controle/Controle.java
+++ aulaJAVA/demo/src/main/java/br/com/example/api/demo/controle/Controle.java
@@ -3,11 +3,17 @@ package br.com.example.api.demo.controle;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 
 
 @RestController
 public class Controle {
+    public Pessoa pessoa(@RequestBody Pessoa p) {
+        return p;
+    }
     
     @GetMapping("")
     public String mensagem() {
