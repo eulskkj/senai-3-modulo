81a03d08d76dbd3ba0b03d5b7f3bfcbc7eac604b
Luis <pikapaybeludo@gmail.com>
2026-06-11 10:27:18 -0300

    Delete aulaJAVA/api directory

--- aulaJAVA/api/src/main/java/com/example/api/repositorio/Repositorio.java
+++
@@ -1,9 +0,0 @@
-package com.example.api.repositorio;
-
-import org.springframework.data.repository.CrudRepository;
-import com.example.api.modelo.Pessoa;
-
-public interface Repositorio extends
-CrudRepository<Pessoa, Integer> {
-    
-}
